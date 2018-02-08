024d7e27b890b2b38a9693935dce10a3890fa97c
Sayandeep <sayandeepl63@gmail.com>
2018-02-09 00:46:23 +0530

    Updated Trainschedule Adapter and Data class.

--- app/src/main/java/com/pronoymukherjee/indianrailwaysenquiry/TrainScheduleAdapter.java
+++ app/src/main/java/com/pronoymukherjee/indianrailwaysenquiry/TrainScheduleAdapter.java
@@ -3,7 +3,12 @@ package com.pronoymukherjee.indianrailwaysenquiry;
 import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,5 +20,28 @@ public class TrainScheduleAdapter extends ArrayAdapter {
     ArrayList<TrainScheduleData> routeList;
     public TrainScheduleAdapter(Activity context,ArrayList<TrainScheduleData> routeList) {
         super(context, 0,routeList);
+        this.routeList=routeList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItem=convertView;
+        if(listItem==null)
+            listItem= LayoutInflater.from(getContext()).inflate(R.layout.custom_train_schedule_list,parent,false);
+        TextView stationName=listItem.findViewById(R.id.tvStationName);
+        TextView distance=listItem.findViewById(R.id.tvDistance);
+        TextView arrival=listItem.findViewById(R.id.tvArrivalTime);
+        TextView halt=listItem.findViewById(R.id.tvHaltTime);
+        TextView departure=listItem.findViewById(R.id.tvDepartureTime);
+        TextView days=listItem.findViewById(R.id.tvDay);
+        TrainScheduleData trainScheduleData=routeList.get(position);
+        stationName.setText(trainScheduleData.getStationName());
+        distance.setText(trainScheduleData.getDistance());
+        arrival.setText(trainScheduleData.getArrival());
+        halt.setText(trainScheduleData.getHalt());
+        departure.setText(trainScheduleData.getDeparture());
+        days.setText(trainScheduleData.getDays());
+        return listItem;
     }
 }
--- app/src/main/java/com/pronoymukherjee/indianrailwaysenquiry/TrainScheduleData.java
+++ app/src/main/java/com/pronoymukherjee/indianrailwaysenquiry/TrainScheduleData.java
@@ -5,5 +5,39 @@ package com.pronoymukherjee.indianrailwaysenquiry;
  */
 
 public class TrainScheduleData {
+    String stationName,arrival,departure;
+    int halt,days,distance;
+    public TrainScheduleData(String stationName,String arrival,String departure,int halt,int days,int distance)
+    {
+        this.stationName=stationName;
+        this.distance=distance;
+        this.arrival=arrival;
+        this.departure=departure;
+        this.halt=halt;
+        this.days=days;
+    }
 
+    public String getStationName() {
+        return stationName;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public int getHalt() {
+        return halt;
+    }
 }
