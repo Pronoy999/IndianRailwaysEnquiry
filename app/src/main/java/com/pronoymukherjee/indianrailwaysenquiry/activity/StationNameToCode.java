a4da17a3edf693aac794ae85bfdf219714b6d5a3
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2018-02-15 20:24:26 +0530

    Added the Station to Code Activity and made a new Shape.

---
+++ app/src/main/java/com/pronoymukherjee/indianrailwaysenquiry/activity/StationNameToCode.java
@@ -0,0 +1,15 @@
+package com.pronoymukherjee.indianrailwaysenquiry.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pronoymukherjee.indianrailwaysenquiry.R;
+
+public class StationNameToCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_station_name_to_code);
+    }
+}
